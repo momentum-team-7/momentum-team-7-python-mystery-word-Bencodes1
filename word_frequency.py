facdf03383bf6ee89dfa7c008f6ecfaef56453a5
Bencodes1 <256.ben@gmail.com>
2021-02-17 08:11:23 -0500

    Got it all working. But I added a new .txt file to test it on that and got a strange error.

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,6 @@
+
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -7,7 +10,37 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    # pass
+    with open(file, 'r') as txt_file:
+        words_temp = txt_file.read()
+        words_list = words_temp.lower()
+        words_list = words_list.split()
+        words_dict = {}
+
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        for ele in words_list:
+            if ele in punctuations:
+                words_list = words_list.replace(ele, "")
+                words_list = words_list.replace("\n", " ")
+
+        words_list_2 = words_list.copy()
+
+        for word in words_list:
+            if word in STOP_WORDS:
+                words_list_2.remove(word)
+            elif word not in words_dict:
+                temp_counter = words_list_2.count(word)
+                words_dict[word]   = temp_counter    
+                                    
+        ranked_words = sorted(words_dict.items(), key = lambda kv: kv[1], reverse=True)
+
+        # print(ranked_words)
+        for key, value in ranked_words:
+            if value > 2:
+                asterisks = value*'*'
+                print('{:>8s}{:^2s}{:^3d}{:<29s}'.format(key, " |", value, asterisks))
+            elif value <= 2:
+                break    
 
 
 if __name__ == "__main__":
